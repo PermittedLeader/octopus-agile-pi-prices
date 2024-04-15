ae425dcf1cc730a749c834768e26791f209a5683
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-04-15 16:36:39 +0100

    Update octoprice_portrait_inky.py

--- octoprice_portrait_inky.py
+++ octoprice_portrait_inky.py
@@ -35,7 +35,7 @@ except sqlite3.OperationalError as error:
     raise SystemExit('Database not found - you need to run store_prices.py first.') from error
 
 inky_display.set_border(inky_display.WHITE)
-img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+img = Image.new("P", (inky_display.HEIGHT,inky_display.WIDTH))
 draw = ImageDraw.Draw(img)
 
 # find current time and convert to year month day etc
